7066e90cf9517dd0be948c3ed7ff6a4cfd85feef
AbdulRehman444 <76523027+AbdulRehman444@users.noreply.github.com>
2021-07-07 12:42:15 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,21 @@
+class Particle {
+    constructor(x,y){
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+    }
+
+    display() {
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+
+        if(frameCount%60 === 0){
+            particles.push(new Particle(random(width/2-10, width/2+10), 10,10));
+        }
+    }
+}
